ee80046c077c34cebc03b6f4b2d7295ecb477ebc
Gabriel Sol <gabsolsilva@gmail.com>
2019-08-30 11:09:30 -0300

    inicio

--- Aula_pratica_1/src/aula_pratica_1/Aula_pratica_1.java
+++ Aula_pratica_1/src/aula_pratica_1/Aula_pratica_1.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package aula_pratica_1;
+import java.util.Scanner;
 
 /**
  *
@@ -17,8 +18,11 @@ public class Aula_pratica_1 { // nome da classe publica deve ser o mesmo do arqu
      */
     public static void main(String[] args) {
         System.out.println("Digite 1 para Cadastrar um Usuario,\n"
-                + "Digite 2 para Cadastrar uma Midia ou\n"
-                + "Digite 3 para Busca ou Emprestimo: ");  
+                            + "Digite 2 para Cadastrar uma Midia ou\n"
+                            + "Digite 3 para Busca ou Emprestimo: ");  
+        Scanner selecao_inicial = new Scanner (System.in);
+        String escolha_inicial = selecao_inicial.nextLine();
+        System.out.println("Escolha inicial " + escolha_inicial);
         // TODO code application logic here
     }
     
