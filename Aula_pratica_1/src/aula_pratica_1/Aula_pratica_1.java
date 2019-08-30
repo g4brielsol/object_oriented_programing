77ad9567428791b660a96cd2069d2ba5d2c04d71
Gabriel Sol <gabsolsilva@gmail.com>
2019-08-30 09:56:30 -0300

    inicio

---
+++ Aula_pratica_1/src/aula_pratica_1/Aula_pratica_1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula_pratica_1;
+
+/**
+ *
+ * @author gabsolsilva
+ */
+public class Aula_pratica_1 { // nome da classe publica deve ser o mesmo do arquivo
+
+    //private String 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Digite 1 para Cadastrar um Usuario,\n"
+                + "Digite 2 para Cadastrar uma Midia ou\n"
+                + "Digite 3 para Busca ou Emprestimo: ");  
+        // TODO code application logic here
+    }
+    
+}
