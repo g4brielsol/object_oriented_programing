78813b0c4bf93bba0128a624f375296a8ae82bad
Gabriel Sol <gabsolsilva@gmail.com>
2019-10-23 23:50:21 -0300

    comeco

---
+++ Trabalho_final/Trabalho_final/src/trabalho_final/Trabalho_final.java
@@ -0,0 +1,18 @@
+package trabalho_final;
+
+import javax.swing.JFrame;
+
+public class Trabalho_final 
+{
+
+    public static void main(String[] args) 
+    {
+        Trab_interface interface_grafica = new Trab_interface();
+        interface_grafica.setBounds(0, 0, 700, 500);
+        interface_grafica.setTitle("Docs");
+        interface_grafica.setVisible(true);
+        interface_grafica.setResizable(false);
+        interface_grafica.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
