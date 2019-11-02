2cd3056580bf4b133e662b8485c24d5a07398be9
Gabriel Sol <gabsolsilva@gmail.com>
2019-11-01 22:58:39 -0300

    Fase 2 completa

--- Trabalho_final/Trabalho_final/src/trabalho_final/Trab_interface.java
+++ Trabalho_final/Trabalho_final/src/trabalho_final/Trab_interface.java
@@ -1,48 +1,55 @@
 package trabalho_final;
 
+import java.awt.FileDialog;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.StringSelection;
 import java.awt.datatransfer.Transferable;
+import java.io.FileWriter;
 import javax.swing.event.UndoableEditEvent;
 import javax.swing.event.UndoableEditListener;
 import javax.swing.undo.UndoManager;
 
 public class Trab_interface extends javax.swing.JFrame {
     // cria objeto da classe Clipboard para transferir dados de copiar e colar
-    Clipboard clipboard = getToolkit().getSystemClipboard();   
-    static UndoManager fazer_refazer = new UndoManager();
+    protected Clipboard clipboard = getToolkit().getSystemClipboard();  
+    // cria objeto da classe UndoManager para fazer os Undos e os Redos
+    protected static UndoManager fazer_refazer = new UndoManager();
+    // nome do arquivo a ser salvo
+    protected String nome_arquivo;
     
     public Trab_interface() {
         initComponents();
     }
-
+    
+    // codigo gerado pelo NetBeans ao criar um JFrame
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         painel = new javax.swing.JPanel();
-        redo = new javax.swing.JButton();
-        undo = new javax.swing.JButton();
+        refazer = new javax.swing.JButton();
+        desfazer = new javax.swing.JButton();
         recortar = new javax.swing.JButton();
         colar = new javax.swing.JButton();
         copiar = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         text_area = new javax.swing.JTextArea();
+        salvar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        redo.setText("Redo");
-        redo.addActionListener(new java.awt.event.ActionListener() {
+        refazer.setText("Refazer");
+        refazer.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                redoActionPerformed(evt);
+                refazerActionPerformed(evt);
             }
         });
 
-        undo.setText("Undo");
-        undo.addActionListener(new java.awt.event.ActionListener() {
+        desfazer.setText("Desfazer");
+        desfazer.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                undoActionPerformed(evt);
+                desfazerActionPerformed(evt);
             }
         });
 
@@ -71,6 +78,13 @@ public class Trab_interface extends javax.swing.JFrame {
         text_area.setRows(5);
         jScrollPane1.setViewportView(text_area);
 
+        salvar.setText("Salvar");
+        salvar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                salvarActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout painelLayout = new javax.swing.GroupLayout(painel);
         painel.setLayout(painelLayout);
         painelLayout.setHorizontalGroup(
@@ -78,19 +92,21 @@ public class Trab_interface extends javax.swing.JFrame {
             .addGroup(painelLayout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(painelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 745, Short.MAX_VALUE)
                     .addGroup(painelLayout.createSequentialGroup()
-                        .addGap(0, 104, Short.MAX_VALUE)
+                        .addGap(0, 0, Short.MAX_VALUE)
                         .addComponent(copiar, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(colar, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(12, 12, 12)
+                        .addComponent(recortar, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(desfazer, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(recortar, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(undo, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(redo, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 79, Short.MAX_VALUE))
-                    .addComponent(jScrollPane1))
+                        .addComponent(refazer, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(salvar)
+                        .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         painelLayout.setVerticalGroup(
@@ -98,11 +114,12 @@ public class Trab_interface extends javax.swing.JFrame {
             .addGroup(painelLayout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(painelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(undo)
-                    .addComponent(redo)
+                    .addComponent(desfazer)
+                    .addComponent(refazer)
                     .addComponent(recortar)
                     .addComponent(colar)
-                    .addComponent(copiar))
+                    .addComponent(copiar)
+                    .addComponent(salvar))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 340, Short.MAX_VALUE))
         );
@@ -120,22 +137,29 @@ public class Trab_interface extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+    // contador de caracteres ao fazer Undo/Redo
     private int counter = 5;
-    private void undoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_undoActionPerformed
+    // funcao do botao Undo
+    private void desfazerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_desfazerActionPerformed
+        //se for possivel executar o Desfazer
         if(fazer_refazer.canUndo())
         {
+            // executa o Desfazer na quantidade especificada pelo counter
             while(counter != 0)
             {
                 fazer_refazer.undo();
                 counter -=1;
             }
+            // counter volta ao seu valor inicial
             counter = 5;
         }
-    }//GEN-LAST:event_undoActionPerformed
-
-    private void redoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_redoActionPerformed
+    }//GEN-LAST:event_desfazerActionPerformed
+    // funcao do botao Redo
+    private void refazerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_refazerActionPerformed
+        // se for possivel executar o "Refazer"
         if(fazer_refazer.canRedo())
         {
+            // executa o Refazer na quantidade especificada pelo counter
             while(counter != 0)
             {
                 fazer_refazer.redo();
@@ -143,22 +167,27 @@ public class Trab_interface extends javax.swing.JFrame {
             }
             counter = 5;
         }
-    }//GEN-LAST:event_redoActionPerformed
+    }//GEN-LAST:event_refazerActionPerformed
 
     private void recortarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_recortarActionPerformed
         // pega o texto selecionado na caixa de texto escrita pelo usuario
         String string_recortada = text_area.getSelectedText();
         // cria um objeto com a capacidade de transferir a string recortada
         StringSelection string_selecionada = new StringSelection(string_recortada);
+        // coloca a string_selecionada no objeto transferível
         clipboard.setContents(string_selecionada, string_selecionada);
+        // substitui o texto selecionado por vazio ("")
         text_area.replaceRange("", text_area.getSelectionStart(), text_area.getSelectionEnd());
     }//GEN-LAST:event_recortarActionPerformed
 
     private void colarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_colarActionPerformed
         try
         {
+            // retorna um objeto transferível representando o conteudo atual do clipboard
             Transferable texto_copiado = clipboard.getContents(Trab_interface.this);
+            // transforma o objeto transferido em string
             String texto_para_colar = (String) texto_copiado.getTransferData(DataFlavor.stringFlavor);
+            // substitui o texto nas áreas de início e fim especificadas pela string texto_para_colar
             text_area.replaceRange(texto_para_colar, 
                                    text_area.getSelectionStart(),
                                    text_area.getSelectionEnd());
@@ -171,16 +200,49 @@ public class Trab_interface extends javax.swing.JFrame {
     }//GEN-LAST:event_colarActionPerformed
 
     private void copiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_copiarActionPerformed
+        // retorna o texto selecionado
         String string_copiada = text_area.getSelectedText();
+        // cria um string capaz de ser transferida
         StringSelection string_selecionada = new StringSelection(string_copiada);
+        // coloca a string_selecionada no conteudo atual do clipboard, que pode ser transferido
         clipboard.setContents(string_selecionada, string_selecionada);
     }//GEN-LAST:event_copiarActionPerformed
+
+    private void salvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_salvarActionPerformed
+        // cria um janela com a especificacao de salvar
+        FileDialog janela_de_salvar = new FileDialog(Trab_interface.this, "Salvar arquivo ", FileDialog.SAVE);
+        janela_de_salvar.setVisible(true);
+        
+        // se o arquivo nao for nulo, coloca o titulo na variavel nome_arquivo
+        if(janela_de_salvar.getFile() != null)
+        {
+            // concatena o nome do diretorio com o nome doa arquivo
+            nome_arquivo = janela_de_salvar.getDirectory() + janela_de_salvar.getFile();
+            // coloca o nome_arquivo no frame
+            setTitle(nome_arquivo);
+        }
+        try
+        {
+            // instancia o objeto que salva arquivos
+            FileWriter salvar_arquivo = new FileWriter(nome_arquivo);
+            // pega o texto escrito na area de texto do programa e salva
+            salvar_arquivo.write(text_area.getText()) ;
+            // colocar o titulo do arquivo
+            setTitle(nome_arquivo);
+            salvar_arquivo.close();
+        }
+        catch (Exception e)
+        {
+            
+        }
+    }//GEN-LAST:event_salvarActionPerformed
 
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
 
+        // Implementado pelo proprio NetBeans ao criar o JFrame
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -197,25 +259,25 @@ public class Trab_interface extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(Trab_interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
-
-        /* Create and display the form */
+        
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Trab_interface().setVisible(true);
             }
         });
     }
-
+    
+    // Implementado pelo proprio NetBeans ao criar o JFrame
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton colar;
     private javax.swing.JButton copiar;
+    protected javax.swing.JButton desfazer;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JPanel painel;
     private javax.swing.JButton recortar;
-    private javax.swing.JButton redo;
+    private javax.swing.JButton refazer;
+    private javax.swing.JButton salvar;
     protected static javax.swing.JTextArea text_area;
-    protected javax.swing.JButton undo;
     // End of variables declaration//GEN-END:variables
 
 }
--- Trabalho_final/Trabalho_final/src/trabalho_final/Trabalho_final.java
+++ Trabalho_final/Trabalho_final/src/trabalho_final/Trabalho_final.java
@@ -3,29 +3,29 @@ package trabalho_final;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.io.IOException;
 import javax.swing.JFrame;
 import javax.swing.event.UndoableEditEvent;
 import javax.swing.event.UndoableEditListener;
 import java.lang.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 public class Trabalho_final 
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
+        
         Trab_interface interface_grafica = new Trab_interface();
         // pega o tamanho da tela do monitor do usuario
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         interface_grafica.setBounds(0, 0, 700, 500);
         // faz a janela do docs ser inicializada no centro da tela do usuario
         interface_grafica.setLocation(dim.width/2-interface_grafica.getSize().width/2, dim.height/2-interface_grafica.getSize().height/2);
-        
+        // coloca o nome na janela
         interface_grafica.setTitle("Docs");
         interface_grafica.setVisible(true);
+        // faz a janela poder ter varios tamanhos
         interface_grafica.setResizable(true);
-        interface_grafica.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // interface nao fazer scroll lateral
         interface_grafica.text_area.setLineWrap(true);
         // undo e redo sem limites de arquivos salvos
@@ -37,10 +37,11 @@ public class Trabalho_final
                 //interface_grafica.fazer_refazer.isSignificant(10);//.isSignificant();
                 public void undoableEditHappened(UndoableEditEvent e)
                 {
-                    interface_grafica.fazer_refazer.addEdit(e.getEdit());
+                        interface_grafica.fazer_refazer.addEdit(e.getEdit());
                 }
            }
         );
+
     }
     
 }
